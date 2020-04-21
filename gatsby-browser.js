3ee71c47f58ea913571279ff35d1319c23dd8252
hoangboobs <p4ssw0rdl4gi>
2020-04-21 14:49:27 +0700

    updated fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 // custom typefaces
+import "typeface-montserrat"
 import "typeface-ibm-plex-sans"
-import "typeface-ibm-plex-serif"
-
 import "prismjs/themes/prism.css"
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,9 +3,9 @@ import theme from 'typography-theme-bootstrap'
 
 theme.baseFontSize = '18px'
 theme.baseLineHeight = 1.75
-theme.headerWeight = 'bold'
-theme.headerFontFamily = ['Georgia', 'serif']
-theme.bodyFontFamily = ['Georgia', 'serif']
+theme.headerWeight = '700'
+theme.headerFontFamily = ['Montserrat', 'sans-serif']
+theme.bodyFontFamily = ['IBM Plex Sans', 'sans-serif']
 theme.overrideThemeStyles = ({ scale }) => ({
   a: {
     color: `rgba(0,0,0,0.77)`,
